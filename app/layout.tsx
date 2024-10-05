0629aeac368c55a69eb61995026a46f9b5b82b86
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-10-05 10:02:00 -0400

    color and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { CartProvider } from 'components/cart/cart-context';
 import { Navbar } from 'components/layout/navbar';
-import { WelcomeToast } from 'components/welcome-toast';
 import { GeistSans } from 'geist/font/sans';
 import { getCart } from 'lib/shopify';
 import { ensureStartsWith } from 'lib/utils';
@@ -43,13 +42,13 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
 
   return (
     <html lang="en" className={GeistSans.variable}>
-      <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+      <body className="bg-neutral-50 text-black selection:bg-teal-300 white:bg-neutral-900 white:text-white white:selection:bg-pink-500 white:selection:text-white">
         <CartProvider cartPromise={cart}>
           <Navbar />
           <main>
             {children}
             <Toaster closeButton />
-            <WelcomeToast />
+            
           </main>
         </CartProvider>
       </body>
