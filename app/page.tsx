5bbb5d5973206f60ce4e945fe09fb68bf7241da1
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-10-05 09:00:29 -0400

    font

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
 import { Carousel } from 'components/carousel';
 import { ThreeItemGrid } from 'components/grid/three-items';
 import Footer from 'components/layout/footer';
-
-export const metadata = {
-  description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
-  openGraph: {
-    type: 'website'
-  }
-};
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Logo.module.css';
 
 export default function HomePage() {
   return (
     <>
+      <nav style={{ position: 'relative', height: '50px' }}>
+        <Link href="/" className={styles.logoLink}>
+          <Image 
+            src="/pictures/asciilogo.png" 
+            alt="Logo" 
+            width={100}
+            height={50}
+            className={styles.logo}
+            priority
+          />
+        </Link>
+      </nav>
       <ThreeItemGrid />
       <Carousel />
       <Footer />
