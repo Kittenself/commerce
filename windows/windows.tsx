a709df330fa0206f03224bc61d385fbf3891708e
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-10-06 02:38:30 -0400

    windows?

---
+++ windows/windows.tsx
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react';
+import { Rnd } from 'react-rnd';
+import styles from '/Windows31.module.css';
+
+interface WindowProps {
+  windowId: string;
+  title: string;
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+}
+
+const Window: React.FC<WindowProps> = ({ windowId, title, children, style }) => {
+  const rndRef = useRef(null);
+
+  return (
+    <Rnd
+      ref={rndRef}
+      default={{
+        x: 100,
+        y: 100,
+        width: 500,
+        height: 'auto',
+      }}
+      bounds="parent"
+      dragHandleClassName={`.${styles.windowHeader}`}
+      className={`window ${styles.window}`}
+      style={{ zIndex: 1, ...style }}
+    >
+      <div className={styles.windowHeader}>
+        <div className={styles.windowClose}></div>
+        <div className={styles.windowTitle}>{title}</div>
+      </div>
+      <div className={styles.windowInner}>{children}</div>
+    </Rnd>
+  );
+};
+
+export default Window;
