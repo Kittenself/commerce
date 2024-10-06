fa715decaea0ad1da7a0fc78ccc4f53da2aa3cc2
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-10-06 17:24:28 -0400

    background and ratio

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,11 @@ import { WindowProvider } from 'windows/windowcontext';
 export default function HomePage() {
   return (
     <WindowProvider>
-    <Navbar />
-
-      <main style={{ minHeight: 'calc(100vh - 100px)' }}>
+      <Navbar />
+      <main className="bg-cover bg-center" style={{ minHeight: 'calc(100vh - 100px)', backgroundImage: "url('public/pictures/vhm_1725304893.png)" }}>
         <WindowContent />
       </main>
-      
       <Footer />
     </WindowProvider>
-    
   );
 }
