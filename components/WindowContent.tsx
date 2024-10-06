e19dd41594933ac70270a4faae8f7e820c5a56e6
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-10-06 17:17:06 -0400

    Update WindowContent.tsx

--- components/WindowContent.tsx
+++ components/WindowContent.tsx
@@ -5,12 +5,21 @@ import Window from 'windows/windows';
 
 const WindowContent: React.FC = () => {
   return (
-    <Window windowId="sampleWindow" title="Sample Window">
-      <div>
-        <p>This is a sample window content.</p>
-        <button onClick={() => alert('Hello!')}>Click Me</button>
-      </div>
-    </Window>
+    <>
+      <Window windowId="sampleWindow" title="Sample Window">
+        <div>
+          <p>This is a sample window content.</p>
+          <button onClick={() => alert('Hello!')}>Click Me</button>
+        </div>
+      </Window>
+
+      <Window windowId="anotherWindow" title="Another Window"> {/* New window added */}
+        <div>
+          <p>This is another window content.</p>
+          <button onClick={() => alert('Hello from another window!')}>Click Me Too</button>
+        </div>
+      </Window>
+    </>
   );
 };
 
