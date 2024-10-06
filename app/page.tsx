28fb4d977aac06b01b10d7f7c67804690cce0955
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-10-06 17:31:07 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,16 @@ export default function HomePage() {
   return (
     <WindowProvider>
       <Navbar />
-      <main className="flex flex-col min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/pictures/vhm_1725304893.png')" }}>
+      <main 
+        className="flex flex-col min-h-screen" 
+        style={{ 
+          backgroundImage: "url('/pictures/vhm_1725304893.png')", 
+          backgroundSize: 'contain', // Ensure the image is at full resolution
+          backgroundRepeat: 'no-repeat', // Prevent the image from repeating
+          backgroundPosition: 'center', // Center the image
+          flexGrow: 1 // Allow the main content to grow and push the footer down
+        }}
+      >
         <div className="flex-grow">
           <WindowContent />
         </div>
