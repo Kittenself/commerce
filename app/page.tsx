f463aaf31e5656ae9b766327656e1ed53edf00fe
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-10-06 17:45:26 -0400

    reverse

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,14 @@ import { WindowProvider } from 'windows/windowcontext';
 export default function HomePage() {
   return (
     <WindowProvider>
-      <Navbar />
-      <main 
-        className="flex flex-col min-h-screen" 
-        style={{ 
-          backgroundImage: "url('/pictures/vhm_1725304893.png')", 
-          backgroundSize: 'contain', // Ensure the image is at full resolution
-          backgroundRepeat: 'no-repeat', // Prevent the image from repeating
-          backgroundPosition: 'center', // Center the image
-          flex: '1 0 auto' // Allow the main content to grow but not push the footer down
-        }}
-      >
-        <div className="flex-grow">
-          <WindowContent />
-        </div>
+    <Navbar />
+
+      <main style={{ minHeight: 'calc(100vh - 100px)' }}>
+        <WindowContent />
       </main>
+      
       <Footer />
     </WindowProvider>
+    
   );
 }
