25f9c313aa27359dc1643c6e4a12c01f51de07a5
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-10-12 23:30:16 -0400

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,4 +1,4 @@
 import localFont from 'next/font/local'
 
-export const DisposableDroidBB = localFont({ src: './fonts/DisposableDroidBB_ital.ttf' })
-export const jacquard12 = localFont({ src: './fonts/jacquard12-Regular.ttf' })
+export const DisposableDroidBB = localFont({ src: '../public/fonts/DisposableDroidBB_ital.ttf' })
+export const jacquard12 = localFont({ src: '../public/fonts/jacquard12-Regular.ttf' })
