0b787ced82a090b8ab077490f8852dd33b119ae4
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-11-10 21:48:07 -0500

    Revert "page transition test"
    This reverts commit f83eb4b5367a2b3ba945553811594a645bf9b7e8.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import { CartProvider } from 'components/cart/cart-context';
-import PageTransition from 'components/PageTransition';
 import { GeistSans } from 'geist/font/sans';
 import { getCart } from 'lib/shopify';
 import { ensureStartsWith } from 'lib/utils';
@@ -43,12 +42,10 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
     <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 white:bg-neutral-900 white:text-white white:selection:bg-pink-500 white:selection:text-white">
         <CartProvider cartPromise={cart}>
-          <PageTransition>
-            <main>
-              {children}
-              <Toaster closeButton />
-            </main>
-          </PageTransition>
+          <main>
+            {children}
+            <Toaster closeButton />
+          </main>
         </CartProvider>
       </body>
     </html>
