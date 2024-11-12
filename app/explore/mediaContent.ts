79f7bc3170a01079268ca566921f1c2f33ad67a9
Kittenself <140145244+Kittenself@users.noreply.github.com>
2024-11-12 13:30:44 -0500

    resize

--- app/explore/mediaContent.ts
+++ app/explore/mediaContent.ts
@@ -14,40 +14,55 @@ export const mediaContents: MediaContent[] = [
     type: 'text',
     content: 'TESTING TESTING TESTING TESTING TESTING TESTING MEOW MEOW MEOW',
     title: 'meow meow meow',
-    initialPosition: { x: 50, y: 50 },
-    initialSize: { width: 300, height: 200 },
+    initialPosition: { x: window?.innerWidth > 768 ? 50 : 20, y: 50 },
+    initialSize: { 
+      width: window?.innerWidth > 768 ? 300 : 280, 
+      height: window?.innerWidth > 768 ? 200 : 150 
+    },
   },
   {
     id: 'image1',
     type: 'gif',
     content: '/pictures/animesher.com_madoka-magica-manga-ascii-699264.gif',
     title: 'Sample Image',
-    initialPosition: { x: 400, y: 100 },
-    initialSize: { width: 400, height: 300 },
+    initialPosition: { x: window?.innerWidth > 768 ? 400 : 20, y: window?.innerWidth > 768 ? 100 : 220 },
+    initialSize: { 
+      width: window?.innerWidth > 768 ? 400 : 280, 
+      height: window?.innerWidth > 768 ? 300 : 200 
+    },
   },
   {
     id: 'gif1',
     type: 'gif',
     content: '/pictures/tumblr_mj7qs6yQ7u1s6mbrfo1_500.gif',
     title: 'Funny GIF',
-    initialPosition: { x: 100, y: 400 },
-    initialSize: { width: 250, height: 250 },
+    initialPosition: { x: window?.innerWidth > 768 ? 100 : 20, y: window?.innerWidth > 768 ? 400 : 440 },
+    initialSize: { 
+      width: window?.innerWidth > 768 ? 250 : 280, 
+      height: window?.innerWidth > 768 ? 250 : 250 
+    },
   },
   {
     id: 'page1',
     type: 'page',
-    content: '/shop',  // This will load the /shop page
+    content: '/shop',
     title: 'Latest release',
-    initialPosition: { x: 700, y: 50 },
-    initialSize: { width: 600, height: 400 },
+    initialPosition: { x: window?.innerWidth > 768 ? 700 : 20, y: window?.innerWidth > 768 ? 50 : 660 },
+    initialSize: { 
+      width: window?.innerWidth > 768 ? 600 : 280, 
+      height: window?.innerWidth > 768 ? 900 : 400 
+    },
   },
   {
     id: 'youtube1',
     type: 'youtube',
-    content: '2pyops3UGr4', // YouTube video ID
+    content: '2pyops3UGr4',
     title: 'im god',
-    initialPosition: { x: 300, y: 300 },
-    initialSize: { width: 560, height: 315 },
+    initialPosition: { x: window?.innerWidth > 768 ? 300 : 20, y: window?.innerWidth > 768 ? 300 : 880 },
+    initialSize: { 
+      width: window?.innerWidth > 768 ? 560 : 280, 
+      height: window?.innerWidth > 768 ? 315 : 200 
+    },
   },
   // Add more media content items as needed
 ];
